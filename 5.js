60efb750d5044577ade06b09225b911520ed050f
dykyys <dykyy.s@gmail.com>
2021-10-02 21:48:16 +0300

    add console.log

--- 5.js
+++ 5.js
@@ -15,20 +15,24 @@ class User {
 
 const mango = new User("Mango");
 // console.log(User.__proto__ === Function.prototype);
-//console.log(User.prototype);
+// console.log(User.prototype);
 // console.log(Function.prototype.__proto__ === Object.prototype);
-// console.log(Object.prototype.__proto__);
+// console.log(Object.prototype);
+//console.log(Object.prototype.__proto__);
 // console.log(User.prototype.__proto__ === Object.prototype);
 
 const arrowAdd = () => {};
 // console.log(arrowAdd.prototype);
 // console.log(arrowAdd.__proto__);
 // console.log(Function.prototype.__proto__ === Object.prototype);
-// console.log(Function.prototype.__proto__.__proto__ === Object.prototype.__proto__);
+// console.log(
+//   Function.prototype.__proto__.__proto__ === Object.prototype.__proto__
+// );
 
-// function Component() {}
-// function Component1() {}
+function Component() {}
+function Component1() {}
 // console.log(Component.prototype === Component1.prototype);
+// console.log(Component.prototype.__proto__ === Object.prototype);
 const age = 21;
 // console.log(age.__proto__.__proto__.__proto__ === Object.prototype.__proto__);
 // console.log(Number.prototype.__proto__ === Object.prototype);
